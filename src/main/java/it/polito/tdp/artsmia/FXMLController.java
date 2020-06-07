cf3b06b95e5a4e9cf8b69d09ef0ff5040c6442e0
s244926 <s244926@studenti.polito.it>
2020-06-08 01:24:54 +0200

    Progetto Base

---
+++ src/main/java/it/polito/tdp/artsmia/FXMLController.java
@@ -0,0 +1,72 @@
+package it.polito.tdp.artsmia;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.artsmia.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCreaGrafo;
+
+    @FXML
+    private Button btnArtistiConnessi;
+
+    @FXML
+    private Button btnCalcolaPercorso;
+
+    @FXML
+    private ComboBox<?> boxRuolo;
+
+    @FXML
+    private TextField txtArtista;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doArtistiConnessi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCalcolaPercorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Artsmia.fxml'.";
+        assert btnArtistiConnessi != null : "fx:id=\"btnArtistiConnessi\" was not injected: check your FXML file 'Artsmia.fxml'.";
+        assert btnCalcolaPercorso != null : "fx:id=\"btnCalcolaPercorso\" was not injected: check your FXML file 'Artsmia.fxml'.";
+        assert boxRuolo != null : "fx:id=\"boxRuolo\" was not injected: check your FXML file 'Artsmia.fxml'.";
+        assert txtArtista != null : "fx:id=\"txtArtista\" was not injected: check your FXML file 'Artsmia.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Artsmia.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
+
---
+++ src/main/java/it/polito/tdp/artsmia/model/TestModel.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.artsmia.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Model m = new Model();
+	}
+
+}
